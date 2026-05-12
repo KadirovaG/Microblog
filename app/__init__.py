ed053dd097083dbf8c73b3bed531d42373c6a82f
Gulsum <your-email@example.com>
2026-05-12 16:49:41 +0500

    Refined the login page and app structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,18 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from app import routes  # noqa: E402, F401
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+login = LoginManager(app)
+# This tells Flask-Login which view handles logins
+login.login_view = 'login'  # type: ignore
+
+# The bottom import prevents circular dependencies
+from app import routes, models  # noqa: E402, F401
