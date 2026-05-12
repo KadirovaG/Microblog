6aa61caeff9299132e6294c6559b1aa1c4bf31ad
Gulsum <your-email@example.com>
2026-05-12 06:03:09 +0500

    Refined the login page and app structure

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,10 @@
-from flask import redirect, url_for
-
-import app
+from flask import render_template
+from app import app
 from app.forms import LoginForm
 
 # ...
 
-@app.route('/login', methods=['GET', 'POST'])  # type: ignore # noqa: F821
+@app.route('/login')
 def login():
-    form = LoginForm()  # noqa: F821
-    if form.validate_on_submit():
-        # ...
-        return redirect(url_for('index'))
-    # ...
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
