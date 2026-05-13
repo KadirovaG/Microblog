f109fccf0927c1b806f3863bc83557281877857c
Gulsum <your-email@example.com>
2026-05-13 18:41:04 +0500

    ffff

--- app/forms.py
+++ app/forms.py
@@ -33,7 +33,23 @@ class RegistrationForm(FlaskForm):
         if user is not None:
             raise ValidationError('Please use a different email address.')
         
-class EditProfileForm(FlaskForm):
+class EditProfileForm(FlaskForm): # type: ignore
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
+
+class EditProfileForm(FlaskForm):  # noqa: F811
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = db.session.scalar(sa.select(User).where(
+                User.username == username.data))
+            if user is not None:
+                raise ValidationError('Please use a different username.')
