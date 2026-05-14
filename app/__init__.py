b838d8b2dce948986f0425f9c432a29d51339837
Gulsum <your-email@example.com>
2026-05-14 23:05:11 +0500

    ssss

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,18 @@
 from flask import Flask
 from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy # type: ignore
+from flask_migrate import Migrate # type: ignore
+from flask_login import LoginManager # type: ignore
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler # Added RotatingFileHandler
 import os # Added os to create the logs folder
-from flask_mail import Mail
+from flask_mail import Mail # type: ignore
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
 mail = Mail(app)
+moment = Moment(app)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
